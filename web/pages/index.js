1af42eb9331fbe7480b6848928d372351ff90267
Aaron Marsh <aaronmarsh755@gmail.com>
2021-04-18 01:55:23 -0400

    adding next js

---
+++ web/pages/index.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+    <section>
+      <h2> Welcome to Downtown... I guess</h2>
+      </section>
+    </Layout>
+  )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
